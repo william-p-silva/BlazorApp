692b7b5d052e3392e3283db45c2249f38d1929a1
william-p-silva <josepereirawilliam@gmail.com>
2026-04-12 19:38:26 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorApp/Model/Endereco.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Model
+{
+    public class Endereco
+    {
+        public int id { get; set; }
+        public string NomeCliente { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Rua { get; set; } = string.Empty;
+        public int Numero { get; set; }
+    }
+}
---
+++ BlazorApp/Model/Pedido.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Model
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public List<ModeloPizza> Pizzas { get; set; } = new();
+        public string Status { get; set; } = "Em preparo";
+        public Endereco EnderecoEntrega { get; set; } = new();
+        public decimal GetPrecoTotal() => Pizzas.Sum(p => p.GetPrecoTotal());
+    }
+}
